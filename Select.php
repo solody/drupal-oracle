ef1d97d1e9d3df6cfa54a8108a67d68784fac679
bohart <bohart@289861.no-reply.drupal.org>
2020-01-27 16:32:24 +0200

    Issue #3108895 by bohart: Fixed Database\SelectTest::testVulnerableComment.

--- Select.php
+++ Select.php
@@ -83,7 +83,7 @@ class Select extends QuerySelect {
 
     // FROM - We presume all queries have a FROM, as any query that doesn't
     // won't need the query builder anyway.
-    $query .= "\nFROM ";
+    $query .= "\nFROM";
     foreach ($this->tables as $alias => $table) {
       $query .= "\n";
       if (isset($table['join type'])) {
